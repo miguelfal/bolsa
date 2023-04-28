2711b19711dd1988bdca33b483ae30b1cc7d60e9
miguel <miguelfa@protonmail.com>
2023-04-28 23:34:05 +0100

    Completed scrape_cmvm_shortselling.py to export to sqlite.

--- scrape_cmvm_shortselling.py
+++ scrape_cmvm_shortselling.py
@@ -1,132 +1,236 @@
-# importar  libs
-import urllib.request
-import math
-import csv
-import sys
+"""
+To get the information from CMVM on short positions in Lisbon Stock Market and input into SQLite
+database
 
+There are 2 types of information:
+
+1) the short selling aggregate numbers
+2) the short selling communications
+
+"""
+
+# import packages needed
+import urllib.request
+import csv
+import sqlite3
+
+from sqlite3 import Error
 from bs4 import BeautifulSoup
 
-# set default output file name
-output_file_name = 'short_selling_lisboa.csv'
-output_file_name_coms = 'short_selling_lisboa_coms.csv'
 
-# set file name to first argument if exists
-if len(sys.argv)>1:
-    output_file_name = sys.argv[1]
+def get_cmvm_shortselling(type_data: str, output_file_name: str = '') -> list:
+    """
+    Get with the information on short positions by company from the CMVM website
 
-# url inicial para começar
-main_url="https://web3.cmvm.pt/english/sdi/emitentes/shortselling/index.cfm"
-shortselling_url="https://web3.cmvm.pt/english/sdi/emitentes/shortselling/"
+    :args:
+    type:
+        'comm' : positions communicated
+        'agg' : aggregate positions
+    output_file_name:
+        name of csv file to export
 
-# obter página inicial
-client = urllib.request.urlopen(main_url)
-html_page = client.read()
-client.close()
+    :return:
+    list with the information on new communications short positions by company
+    """
 
-# obter html
-page_soup = BeautifulSoup(html_page, "html.parser")
+    # set urls
+    main_url = 'https://web3.cmvm.pt/english/sdi/emitentes/shortselling/'  # list of companies
 
-company_list = page_soup.find_all('article')
+    # get html with list of companies
+    client = urllib.request.urlopen(main_url)
+    html_page = client.read()
+    client.close()
 
-#company_list = company_list[0:2]
-#print(len(company_list))
-#print(company_list)
+    # get html parsed
+    page_soup = BeautifulSoup(html_page, 'html.parser')
 
-data = [] #aggregate positions
-data_coms = [] #communications per entitie
+    # get list of companies
+    company_list = page_soup.find_all('article')
+
+    # initialize list to be returned
+    data = []
+
+    # aggregate data
+    if type_data == 'agg':
+
+        # loop list of companies
+        for company in company_list:  # loop list of companies
+            company_link = company.a['href']  # get links to the page from each company
+            company_name = company.a['title']  # get name of company
+            company_page_link = main_url + company_link  # set full link to open
+            client = urllib.request.urlopen(company_page_link)  # open link
+            html_page = client.read()
+            client.close()
+            company_soup = BeautifulSoup(html_page, 'html.parser')
+            company_file_list = company_soup.select('a[href*=hist_]')
+
+            # loop links
+            for file_link in company_file_list:
+                partial_file_link = file_link['href']
+                final_file_link = main_url + partial_file_link  # get the link to the actual page
+                client = urllib.request.urlopen(final_file_link)
+                html_page = client.read()
+                client.close()
+                file_soup = BeautifulSoup(html_page, 'html.parser')
+                table = file_soup.find('table', {'class': 'WTabela'})
+
+                # get each row from the table on the page, some are empty
+                try:
+                    rows = table.find_all('tr')
+                    for row in rows:
+                        cols = row.find_all('td')
+                        cols = [company_name] + [ele.text.strip() for ele in cols]
+                        if len(cols) == 3 and len(
+                                cols[1]) == 10:  # only those that have a date and all 3 columns filled out
+                            data.append([ele for ele in cols])
+                except AttributeError:  # ignore empty tables
+                    pass
+
+        # decompose list in columns
+        df1 = [item[0] for item in data if item]
+        df2 = [item[1] for item in data if item]
+        df3 = [item[2] for item in data if item]
+
+        # format date on the 2nd column
+        df2 = [sub.replace('/', '-') for sub in df2]
+        df2 = [sub[6:11] + sub[2:5] + '-' + sub[0:2] for sub in df2]
+
+        # combine lists back together
+        data = tuple(zip(df1, df2, df3))
+
+    if type_data == 'comm':
+
+        # loop list of companies
+        for company in company_list:
+            company_link = company.a['href']  # get links to the page from each company
+            company_name = company.a['title']  # get name of company
+            company_page_link = main_url + company_link  # set full link to open
+            client = urllib.request.urlopen(company_page_link)
+            html_page = client.read()
+            client.close()
+            company_soup = BeautifulSoup(html_page, 'html.parser')
+
+            # loop links
+            company_file_list = company_soup.select('a[href*=historico_]')  # select links for the communications
+            for file_link in company_file_list:
+                partial_file_link = file_link['href']
+                final_file_link = main_url + partial_file_link  # get the link to the actual page
+                client = urllib.request.urlopen(final_file_link)
+                html_page = client.read()
+                client.close()
+                file_soup = BeautifulSoup(html_page, 'html.parser')
+                table = file_soup.find('table', {'class': 'WTabela'})
+                try:
+                    rows = table.find_all('tr')
+                    for row in rows:
+                        cols = row.find_all('td')
+                        cols = [company_name] + [ele.text.strip() for ele in cols]
+                        if len(cols) == 5 and len(cols[3]) == 10:  # only those that have a date and all 5 columns
+                            # filled out
+                            data.append([ele for ele in cols])
+                except AttributeError:  # ignore empty tables
+                    pass
+
+        # decompose list
+        df1 = [item[0] for item in data if item]
+        df2 = [item[1] for item in data if item]
+        df3 = [item[2] for item in data if item]
+        df4 = [item[3] for item in data if item]
+        df5 = [item[4] for item in data if item]
+
+        # format date on 3rd and 4th columns
+        df4 = [sub.replace('/', '-') for sub in df4]
+        df4 = [sub[6:11] + sub[2:5] + '-' + sub[0:2] for sub in df4]
+        df5 = [sub.replace('/', '-') for sub in df5]
+        df5 = [sub[6:11] + sub[2:5] + '-' + sub[0:2] for sub in df5]
+
+        # combine lists back together
+        data = tuple(zip(df1, df2, df3, df4, df5))
+
+    if len(output_file_name) > 0:
+        if type_data == 'agg':
+            column_names = ['company', 'date', '% of capital']
+        else:
+            column_names = ['company', 'holder of position', '% of capital', 'communication date', 'position date']
+
+        with open(output_file_name, 'w') as f:
+            write = csv.writer(f)
+            write.writerow(column_names)
+            write.writerows(data)
+
+    return data
 
 
-for company in company_list:
-     company_link = company.a['href']
-     company_name = company.a['title']
-     #print(company_name)
-     company_page_link = shortselling_url + company_link
-     client = urllib.request.urlopen(company_page_link)
-     html_page = client.read()
-     client.close()
-     company_soup =  BeautifulSoup(html_page, "html.parser")
-     company_file_list = company_soup.select("a[href*=hist_]")
-     for file_link in company_file_list:
-         partial_file_link= file_link['href']
-         final_file_link = shortselling_url + partial_file_link
-         client = urllib.request.urlopen(final_file_link)
-         html_page = client.read()
-         client.close()
-         file_soup = BeautifulSoup(html_page, "html.parser")
-         table = file_soup.find('table', {"class": "WTabela"})
-         try:
-             rows = table.find_all('tr')
-             for row in rows:
-                 cols = row.find_all('td')
-                 cols = [company_name] + [ele.text.strip() for ele in cols]
-                 if len(cols)==3 and len(cols[1])==10: # Get rid of empty values
-                        data.append([ele for ele in cols])
-         except AttributeError: #ignore empty tables>
-             pass
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
 
-     company_file_list = company_soup.select("a[href*=historico_]")
-     for file_link in company_file_list:
-         partial_file_link = file_link['href']
-         final_file_link = shortselling_url + partial_file_link
-         client = urllib.request.urlopen(final_file_link)
-         html_page = client.read()
-         client.close()
-         file_soup = BeautifulSoup(html_page, "html.parser")
-         table = file_soup.find('table', {"class": "WTabela"})
-         try:
-             rows = table.find_all('tr')
-             for row in rows:
-                 cols = row.find_all('td')
-                 cols = [company_name] + [ele.text.strip() for ele in cols]
-                 if len(cols) == 5 and len(cols[3])==10:  # Get rid of empty values
-                     data_coms.append([ele for ele in cols])
-         except AttributeError:  # ignore empty tables>
-             pass
+    return conn
 
-# decompose list
-df1 = [item[0] for item in data if item]
-df2 = [item[1] for item in data if item]
-df3 = [item[2] for item in data if item]
 
-# format date
-df2 = [sub.replace('/', '-') for sub in df2]
-df2 = [sub[6:11]+sub[2:5]+'-'+sub[0:2] for sub in df2]
+def insert_short_agg(conn: sqlite3, data: list) -> int:
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param data:
+    :return: project id
+    """
+    cur = conn.cursor()
+    sql = 'DELETE FROM short_selling_agg'
+    cur.execute(sql)
 
-# combine lists
-data = tuple(zip(df1, df2, df3))
+    sql = 'INSERT INTO short_selling_agg(company,date,p_of_capital) VALUES(?,?,?)'
 
-column_names = ['company', 'date', '% of capital']
-with open(output_file_name, 'w') as f:
-    write = csv.writer(f)
-    write.writerow(column_names)
-    write.writerows(data)
+    for item in data:
+        cur.execute(sql, item)
+    cur.close()
+    conn.commit()
+    return cur.lastrowid
 
-# decompose list
-df1 = [item[0] for item in data_coms if item]
-df2 = [item[1] for item in data_coms if item]
-df3 = [item[2] for item in data_coms if item]
-df4 = [item[3] for item in data_coms if item]
-df5 = [item[4] for item in data_coms if item]
 
-# format date
-df4 = [sub.replace('/', '-') for sub in df4]
-df4 = [sub[6:11]+sub[2:5]+'-'+sub[0:2] for sub in df4]
+def insert_short_comm(conn: sqlite3, data: list) -> int:
+    """
+    Create a new task
+    :param conn:
+    :param data:
+    :return:
+    """
 
-df5 = [sub.replace('/', '-') for sub in df5]
-df5 = [sub[6:11]+sub[2:5]+'-'+sub[0:2] for sub in df5]
+    cur = conn.cursor()
+    sql = 'DELETE FROM short_selling_comm'
+    cur.execute(sql)
+    sql = 'INSERT INTO short_selling_comm(company,holder_of_position,p_of_capital,communication_date,position_date) ' \
+          'VALUES(?,?,?,?,?)'
 
-# combine lists
-data_coms = tuple(zip(df1, df2, df3, df4, df5))
+    for item in data:
+        cur.execute(sql, item)
+    cur.close()
+    conn.commit()
 
-column_names = ['company', 'holder of position', '% of capital', 'communication date', 'position date']
-with open(output_file_name_coms, 'w') as f:
-    write = csv.writer(f)
-    write.writerow(column_names)
-    write.writerows(data_coms)
+    return cur.lastrowid
 
-# decompose list
-df1 = [item[0] for item in data_coms if item]
-df2 = [item[1] for item in data_coms if item]
-df3 = [item[2] for item in data_coms if item]
-df4 = [item[3] for item in data_coms if item]
-df5 = [item[4] for item in data_coms if item]
+
+def main():
+
+    # set database name
+    database = r'bolsa.db'
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # write to tables
+    with conn:
+        project_id = insert_short_agg(conn, get_cmvm_shortselling('agg'))
+        project_id = insert_short_comm(conn, get_cmvm_shortselling('comm'))
+
+
+if __name__ == '__main__':
+    main()
